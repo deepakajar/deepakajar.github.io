e5c5d96914b14a850d75fc28de0ca7b4c48b401f
deepakraja <103764380+deepakajar@users.noreply.github.com>
2023-08-21 12:41:54 +0530

    initial

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+   $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+   }); 
+
+   $(window).on('scroll load',function(){
+      $('#menu').removeclass('fa-times');
+      $('header').removeclass('toggle');
+      if($(window).screenTop()>0){
+         $('.top').show();
+      }
+      else
+      {
+         $('.top').hide();
+      }
+   });
+   //smooth scrolling
+   $('a[href*="#"]').on('click',function(e){
+
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+
+});
